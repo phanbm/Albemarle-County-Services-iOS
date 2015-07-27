dbc664b8546f5b5fcc89874857f521b59906cac5
phanbm <Bryan@Bryans-MacBook-Pro.local>
2015-07-27 13:17:57 -0400

    ACS iOS 2.0 because we have 2 MacBooks now!
    Layout stuff for start page should be good now.

--- Albemarle County Services iOS/ReportViewController.h
+++ Albemarle County Services iOS/ReportViewController.h
@@ -2,7 +2,7 @@
 //  ReportViewController.h
 //  Albemarle County Services iOS
 //
-//  Created by Bryan Phan on 7/24/15.
+//  Created by Bryan Phan on 7/27/15.
 //  Copyright (c) 2015 Albemarle County IT Department Interns. All rights reserved.
 //
 
